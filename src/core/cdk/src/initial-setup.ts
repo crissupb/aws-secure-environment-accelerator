79f5a2a0a6ae1b8f38b7c35bb6a292e66a49ec24
Brian Crissup <crissupb@amazon.com>
2023-05-02 09:35:05 -0400

    Remove policy statement

--- src/core/cdk/src/initial-setup.ts
+++ src/core/cdk/src/initial-setup.ts
@@ -206,13 +206,13 @@ export namespace InitialSetup {
         maxSessionDuration: buildTimeout,
       });
 
-      pipelineRole.assumeRolePolicy?.addStatements(
-        new iam.PolicyStatement({
-          actions: ['sts:AssumeRole'],
-          effect: iam.Effect.ALLOW,
-          principals: [new iam.ArnPrincipal(roleArn)],
-        }),
-      );
+      // pipelineRole.assumeRolePolicy?.addStatements(
+      //   new iam.PolicyStatement({
+      //     actions: ['sts:AssumeRole'],
+      //     effect: iam.Effect.ALLOW,
+      //     principals: [new iam.ArnPrincipal(roleArn)],
+      //   }),
+      // );
 
       // S3 working bucket
       const s3WorkingBucket = new s3.Bucket(this, 'WorkingBucket', {
